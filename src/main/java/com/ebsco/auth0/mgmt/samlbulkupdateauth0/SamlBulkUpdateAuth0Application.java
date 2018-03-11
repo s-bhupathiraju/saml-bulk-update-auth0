0101ace28a394785806797cf25a053963c83c2c4
Sanjay Bhupathiraju <sbhupathiraju@ebsco.com>
2018-03-10 23:30:03 -0500

    spring boot 2.0 initialized with web flux

---
+++ src/main/java/com/ebsco/auth0/mgmt/samlbulkupdateauth0/SamlBulkUpdateAuth0Application.java
@@ -0,0 +1,12 @@
+package com.ebsco.auth0.mgmt.samlbulkupdateauth0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamlBulkUpdateAuth0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamlBulkUpdateAuth0Application.class, args);
+	}
+}
